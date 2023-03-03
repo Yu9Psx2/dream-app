5bdebbeb3870bd2bbf9c90da11a783d479039949
T <trevor.yeomans@gmail.com>
2023-03-03 10:43:11 -0700

    Initial Cookie Set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
-import AWS from 'aws-sdk';
+import React, { useState, useEffect } from 'react';
 import { invokeLambdaFunction } from './lambdaFunctions';
+import { useCookies } from 'react-cookie';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
   const [isLoading, setIsLoading] = useState(false)
+  const [cookies, setCookie] = useCookies(['id']);
+  const oneDay = 60 * 60 * 24; // 1 day in seconds
+
+
+  useEffect(() => {
+    // Check if the cookie exists
+    if (!cookies.id) {
+      // If the cookie doesn't exist, generate a new user ID and set the cookie
+      const userId = uuidv4();
+      setCookie('id', userId, { expires: new Date(Date.now() + oneDay) });
+    } else {
+      // If the cookie exists, retrieve the user ID from the cookie and use it in your application
+      const userId = cookies.id;
+      console.log('User ID:', userId);
+    }
+  }, [cookies, setCookie]);
+
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -23,6 +41,7 @@ function App() {
 
   return (
     <>
+    <div>This site uses cookies to track your progress.</div>
     <div>
       <form onSubmit={handleFormSubmit}>
         <label>
